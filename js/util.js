a9a10ff02fdb96632a96340c39c6b71bcb787f8b
Maor4214 <153186394+Maor4214@users.noreply.github.com>
2024-09-22 19:14:27 +0300

    1st upload

---
+++ js/util.js
@@ -0,0 +1,72 @@
+'use strict'
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function makeId(length = 6) {
+    var txt = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
+}
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF'
+    var color = '#'
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+function findEmptyCells() {
+    var emptyCells = []
+
+    for (var i = 1; i < gBoard.length - 1; i++) {
+        for (var j = 1; j < gBoard[0].length - 1; j++) {
+            var currCell = gBoard[i][j]
+
+            if (currCell === EMPTY) {
+                var emptyCell = { i, j }
+                emptyCells.push(emptyCell)
+            }
+        }
+    }
+    return emptyCells
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
+
+function playSound(sound) {
+    const audioPop = new Audio(sound)
+    audioPop.play()
+}
+
+
+function findNegsMines(rowIdx, colIdx, mat) {
+    var closeMinesCount = 0
+    // console.log('rowIdx, collIdx', rowIdx, colIdx)
+
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= mat.length) continue
+
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (j < 0 || j >= mat[0].length) continue
+            if (i === rowIdx && j === colIdx) continue
+            if (mat[i][j].isMine) closeMinesCount++
+            
+        }
+    }
+    return closeMinesCount
+}
